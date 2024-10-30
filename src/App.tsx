fffb97988714822fa8d0d5414d5f8ae7dcc00fc7
ElodieMons <emons47110@gmail.com>
2024-10-30 16:41:12 +0100

    Quest Part 05 modif - Pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,64 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
-
+import { useState } from "react";
 
 
 
 
 function App() {
 
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
   const pokemonList = [
     {
-      name: "Bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      name: "bulbasaur",
+      imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
-      name: "Mew",
-
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/otherofficial-artwork/25.png",
+    },
+    {
+      name: "mew",
     },
   ];
-  
-  
+
+
+
+  const handleClickNext = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+  const handleClickPrevious = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
 
   return (
-    <div>{
-      pokemonList.map((pokemon) => {
-        return (
-          <PokemonCard 
-            name={pokemon.name}
-            imgSrc={pokemon.imgSrc}
-          />
-        )
-      })
-      }
-    
-    </div>
-  );
-}
+		<div className="container">
+			<PokemonCard pokemon={pokemonList[pokemonIndex]} />
 
+			<div>
+				{pokemonIndex > 0 ? (
+					<button type="button" onClick={handleClickPrevious}>Précédent</button>) : null}
+
+				{pokemonIndex < pokemonList.length - 1 ? (
+					<button type="button" onClick={handleClickNext}>Suivant</button>) : null}
+			</div>
+      
+		</div>
+	);
+
+}
 
 export default App;
