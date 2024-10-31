07c6ebd018476caaeea560eadc5b9974db6d764b
ElodieMons <emons47110@gmail.com>
2024-10-31 10:50:11 +0100

    modif2

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Mew",
-  },
-];
+
 
 
 function App() {
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "Mew",
+    },
+  ];
+
+  
   return (
     <div>
       <PokemonCard pokemon={pokemonList[0]}/>
